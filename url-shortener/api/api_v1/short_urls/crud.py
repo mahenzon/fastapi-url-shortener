f1282580535ee0441be163ddeaaaed6764c0e023
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-01 14:39:01 +0300

    create ShortUrlsStorage

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -1,12 +1,46 @@
-from schemas.short_url import ShortUrl
+"""
+Create
+Read
+Update
+Delete
+"""
 
-SHORT_URLS = [
-    ShortUrl(
-        target_url="https://www.example.com",
+from pydantic import BaseModel, AnyHttpUrl
+
+from schemas.short_url import (
+    ShortUrl,
+    ShortUrlCreate,
+)
+
+
+class ShortUrlsStorage(BaseModel):
+    slug_to_short_url: dict[str, ShortUrl] = {}
+
+    def get(self) -> list[ShortUrl]:
+        return list(self.slug_to_short_url.values())
+
+    def get_by_slug(self, slug: str) -> ShortUrl | None:
+        return self.slug_to_short_url.get(slug)
+
+    def create(self, short_url_in: ShortUrlCreate) -> ShortUrl:
+        short_url = ShortUrl(
+            **short_url_in.model_dump(),
+        )
+        self.slug_to_short_url[short_url.slug] = short_url
+        return short_url
+
+
+storage = ShortUrlsStorage()
+
+storage.create(
+    ShortUrlCreate(
+        target_url=AnyHttpUrl("https://www.example.com"),
         slug="example",
     ),
-    ShortUrl(
-        target_url="https://www.google.com",
+)
+storage.create(
+    ShortUrlCreate(
+        target_url=AnyHttpUrl("https://www.google.com"),
         slug="search",
     ),
-]
+)
--- url-shortener/api/api_v1/short_urls/views.py
+++ url-shortener/api/api_v1/short_urls/views.py
@@ -11,7 +11,7 @@ from schemas.short_url import (
     ShortUrlCreate,
 )
 
-from .crud import SHORT_URLS
+from .crud import storage
 from .dependencies import (
     prefetch_short_url,
 )
@@ -26,8 +26,8 @@ router = APIRouter(
     "/",
     response_model=list[ShortUrl],
 )
-def read_short_urls_list():
-    return SHORT_URLS
+def read_short_urls_list() -> list[ShortUrl]:
+    return storage.get()
 
 
 @router.post(
@@ -37,10 +37,8 @@ def read_short_urls_list():
 )
 def create_short_url(
     short_url_create: ShortUrlCreate,
-):
-    return ShortUrl(
-        **short_url_create.model_dump(),
-    )
+) -> ShortUrl:
+    return storage.create(short_url_create)
 
 
 @router.get(
