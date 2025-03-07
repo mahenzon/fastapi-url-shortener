2d58fecef3076dccbc8ed1abe214bdfb793345f6
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-07 22:42:18 +0300

    Save state to .json file

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -5,8 +5,9 @@ Update
 Delete
 """
 
-from pydantic import BaseModel, AnyHttpUrl
+from pydantic import BaseModel, ValidationError
 
+from core.config import SHORT_URLS_STORAGE_FILEPATH
 from schemas.short_url import (
     ShortUrl,
     ShortUrlCreate,
@@ -17,6 +18,15 @@ from schemas.short_url import (
 
 class ShortUrlsStorage(BaseModel):
     slug_to_short_url: dict[str, ShortUrl] = {}
+
+    def save_state(self) -> None:
+        SHORT_URLS_STORAGE_FILEPATH.write_text(self.model_dump_json(indent=2))
+
+    @classmethod
+    def from_state(cls) -> "ShortUrlsStorage":
+        if not SHORT_URLS_STORAGE_FILEPATH.exists():
+            return ShortUrlsStorage()
+        return cls.model_validate_json(SHORT_URLS_STORAGE_FILEPATH.read_text())
 
     def get(self) -> list[ShortUrl]:
         return list(self.slug_to_short_url.values())
@@ -29,10 +39,12 @@ class ShortUrlsStorage(BaseModel):
             **short_url_in.model_dump(),
         )
         self.slug_to_short_url[short_url.slug] = short_url
+        self.save_state()
         return short_url
 
     def delete_by_slug(self, slug: str) -> None:
         self.slug_to_short_url.pop(slug, None)
+        self.save_state()
 
     def delete(self, short_url: ShortUrl) -> None:
         self.delete_by_slug(slug=short_url.slug)
@@ -44,6 +56,7 @@ class ShortUrlsStorage(BaseModel):
     ) -> ShortUrl:
         for field_name, value in short_url_in:
             setattr(short_url, field_name, value)
+        self.save_state()
         return short_url
 
     def update_partial(
@@ -53,20 +66,12 @@ class ShortUrlsStorage(BaseModel):
     ):
         for field_name, value in short_url_in.model_dump(exclude_unset=True).items():
             setattr(short_url, field_name, value)
+        self.save_state()
         return short_url
 
 
-storage = ShortUrlsStorage()
-
-storage.create(
-    ShortUrlCreate(
-        target_url=AnyHttpUrl("https://www.example.com"),
-        slug="example",
-    ),
-)
-storage.create(
-    ShortUrlCreate(
-        target_url=AnyHttpUrl("https://www.google.com"),
-        slug="search",
-    ),
-)
+try:
+    storage = ShortUrlsStorage.from_state()
+except ValidationError:
+    storage = ShortUrlsStorage()
+    storage.save_state()
