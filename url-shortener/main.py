73abfa2e5fb45ce126c8a074bc5c37783dd94ea0
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-07 23:11:54 +0300

    add logging to storage actions

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import (
     FastAPI,
     Request,
@@ -5,6 +7,12 @@ from fastapi import (
 
 from api import router as api_router
 from api.redirect_views import router as redirect_views
+from core import config
+
+logging.basicConfig(
+    level=config.LOG_LEVEL,
+    format=config.LOG_FORMAT,
+)
 
 app = FastAPI(
     title="URL Shortener",
