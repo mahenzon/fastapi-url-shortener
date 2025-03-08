41fd026b032408243e956e48a41d7991d69c010c
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-08 14:37:28 +0300

    add lifespan event to recover storage state

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -34,6 +34,22 @@ class ShortUrlsStorage(BaseModel):
             return ShortUrlsStorage()
         return cls.model_validate_json(SHORT_URLS_STORAGE_FILEPATH.read_text())
 
+    def init_storage_from_state(self) -> None:
+        try:
+            data = ShortUrlsStorage.from_state()
+        except ValidationError:
+            self.save_state()
+            log.warning("Rewritten storage file due to validation error.")
+            return
+
+        # мы обновляем свойство напрямую
+        # если будут новые свойства,
+        # то их тоже надо обновить.
+        self.slug_to_short_url.update(
+            data.slug_to_short_url,
+        )
+        log.warning("Recovered data from storage file.")
+
     def get(self) -> list[ShortUrl]:
         return list(self.slug_to_short_url.values())
 
@@ -76,10 +92,4 @@ class ShortUrlsStorage(BaseModel):
         return short_url
 
 
-try:
-    storage = ShortUrlsStorage.from_state()
-    log.warning("Recovered data from storage file.")
-except ValidationError:
-    storage = ShortUrlsStorage()
-    storage.save_state()
-    log.warning("Rewritten storage file due to validation error.")
+storage = ShortUrlsStorage()
