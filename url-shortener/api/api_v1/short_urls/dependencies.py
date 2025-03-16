862e2d5eb9e6c2a8e5eda3f3101bdd1167eea062
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-16 15:38:06 +0300

    check user credentials in redis db

--- url-shortener/api/api_v1/short_urls/dependencies.py
+++ url-shortener/api/api_v1/short_urls/dependencies.py
@@ -15,13 +15,11 @@ from fastapi.security import (
     HTTPBasicCredentials,
 )
 
-from core.config import (
-    USERS_DB,
-)
 from schemas.short_url import ShortUrl
 
 from api.api_v1.auth.services import (
     redis_tokens,
+    redis_users,
 )
 from .crud import storage
 
@@ -109,10 +107,9 @@ def api_token_required_for_unsafe_methods(
 def validate_basic_auth(
     credentials: HTTPBasicCredentials | None,
 ):
-    if (
-        credentials
-        and credentials.username in USERS_DB
-        and USERS_DB[credentials.username] == credentials.password
+    if credentials and redis_users.validate_user_password(
+        username=credentials.username,
+        password=credentials.password,
     ):
         return
 
