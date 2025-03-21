02a690dc05b9d425264cec5ebb383c9e19871f68
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-21 20:59:09 +0300

    update short urls in redis storage

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -59,6 +59,13 @@ class ShortUrlsStorage(BaseModel):
         )
         log.warning("Recovered data from storage file.")
 
+    def save_short_url(self, short_url: ShortUrl) -> None:
+        redis.hset(
+            name=config.REDIS_SHORT_URLS_HASH_NAME,
+            key=short_url.slug,
+            value=short_url.model_dump_json(),
+        )
+
     def get(self) -> list[ShortUrl]:
         return [
             ShortUrl.model_validate_json(value)
@@ -76,11 +83,7 @@ class ShortUrlsStorage(BaseModel):
         short_url = ShortUrl(
             **short_url_in.model_dump(),
         )
-        redis.hset(
-            name=config.REDIS_SHORT_URLS_HASH_NAME,
-            key=short_url.slug,
-            value=short_url.model_dump_json(),
-        )
+        self.save_short_url(short_url)
         log.info("Created short url %s", short_url)
         return short_url
 
@@ -97,6 +100,7 @@ class ShortUrlsStorage(BaseModel):
     ) -> ShortUrl:
         for field_name, value in short_url_in:
             setattr(short_url, field_name, value)
+        self.save_short_url(short_url)
         return short_url
 
     def update_partial(
@@ -106,6 +110,7 @@ class ShortUrlsStorage(BaseModel):
     ):
         for field_name, value in short_url_in.model_dump(exclude_unset=True).items():
             setattr(short_url, field_name, value)
+        self.save_short_url(short_url)
         return short_url
 
 
