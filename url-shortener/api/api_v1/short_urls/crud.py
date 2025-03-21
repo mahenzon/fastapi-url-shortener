819b08b81c963a185928a240c3a56e8c55c310a1
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-21 20:35:17 +0300

    save short url to redis storage on create

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -8,7 +8,9 @@ Delete
 import logging
 
 from pydantic import BaseModel, ValidationError
+from redis import Redis
 
+from core import config
 from core.config import SHORT_URLS_STORAGE_FILEPATH
 from schemas.short_url import (
     ShortUrl,
@@ -18,6 +20,13 @@ from schemas.short_url import (
 )
 
 log = logging.getLogger(__name__)
+
+redis = Redis(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB_SHORT_URLS,
+    decode_responses=True,
+)
 
 
 class ShortUrlsStorage(BaseModel):
@@ -60,7 +69,11 @@ class ShortUrlsStorage(BaseModel):
         short_url = ShortUrl(
             **short_url_in.model_dump(),
         )
-        self.slug_to_short_url[short_url.slug] = short_url
+        redis.hset(
+            name=config.REDIS_SHORT_URLS_HASH_NAME,
+            key=short_url.slug,
+            value=short_url.model_dump_json(),
+        )
         log.info("Created short url %s", short_url)
         return short_url
 
