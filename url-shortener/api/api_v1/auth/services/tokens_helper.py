ad18793aff99493d0f6d566dddbb61d2f81afbc7
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-03-22 15:49:00 +0300

    create "delete token" method

--- url-shortener/api/api_v1/auth/services/tokens_helper.py
+++ url-shortener/api/api_v1/auth/services/tokens_helper.py
@@ -42,6 +42,15 @@ class AbstractTokensHelper(ABC):
         :return:
         """
 
+    @abstractmethod
+    def delete_token(self, token: str) -> None:
+        """
+        Delete token from storage.
+
+        :param token:
+        :return:
+        """
+
     @classmethod
     def generate_token(cls) -> str:
         return secrets.token_urlsafe(16)
