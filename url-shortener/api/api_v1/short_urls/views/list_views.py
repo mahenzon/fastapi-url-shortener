79021653fb234cab25551d5bb70f38bb621434c3
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-05 17:24:14 +0300

    check if short url already exists on create

--- url-shortener/api/api_v1/short_urls/views/list_views.py
+++ url-shortener/api/api_v1/short_urls/views/list_views.py
@@ -2,6 +2,7 @@ from fastapi import (
     APIRouter,
     status,
     Depends,
+    HTTPException,
 )
 
 from api.api_v1.short_urls.dependencies import (
@@ -52,4 +53,10 @@ def read_short_urls_list() -> list[ShortUrl]:
 def create_short_url(
     short_url_create: ShortUrlCreate,
 ) -> ShortUrl:
-    return storage.create(short_url_create)
+    if not storage.get_by_slug(short_url_create.slug):
+        return storage.create(short_url_create)
+
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f"Short URL with slug={short_url_create.slug!r} already exists.",
+    )
