6155f29959bd6a0e3f6e07230325f073307608e4
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-13 16:17:58 +0300

    testing environment

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_crud.py
@@ -1,5 +1,11 @@
 import random
+from os import getenv
 from unittest import TestCase
+
+if getenv("TESTING") != "1":
+    raise OSError(  # noqa: TRY003
+        "Environment is not ready for testing",  # noqa: EM101
+    )
 
 
 def total(a: int, b: int) -> int:
