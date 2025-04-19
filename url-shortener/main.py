68196558ddd74b562bf4e7885bb20f954a02e94d
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-19 14:19:58 +0300

    move main views to a separate router

--- url-shortener/main.py
+++ url-shortener/main.py
@@ -2,10 +2,10 @@ import logging
 
 from fastapi import (
     FastAPI,
-    Request,
 )
 
 from api import router as api_router
+from api.main_views import router as main_router
 from api.redirect_views import router as redirect_views
 from app_lifespan import lifespan
 from core import config
@@ -19,20 +19,7 @@ app = FastAPI(
     title="URL Shortener",
     lifespan=lifespan,
 )
+
+app.include_router(main_router)
 app.include_router(redirect_views)
 app.include_router(api_router)
-
-
-@app.get("/")
-def read_root(
-    request: Request,
-    name: str = "World",
-) -> dict[str, str]:
-    docs_url = request.url.replace(
-        path="/docs",
-        query="",
-    )
-    return {
-        "message": f"Hello {name}",
-        "docs": str(docs_url),
-    }
