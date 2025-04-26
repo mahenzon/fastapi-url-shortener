7ea505870408533b9c1edc161f65a605950e56ec
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-26 13:45:34 +0300

    add parametrized fixture tests for short url create

--- url-shortener/testing/test_api/test_api_v1/test_short_urls/test_views/test_list_views/test_create.py
+++ url-shortener/testing/test_api/test_api_v1/test_short_urls/test_views/test_list_views/test_create.py
@@ -1,11 +1,15 @@
 import random
 import string
+from typing import Any
 
+import pytest
+from _pytest.fixtures import SubRequest
 from fastapi import status
 from fastapi.testclient import TestClient
 
 from main import app
 from schemas.short_url import ShortUrl, ShortUrlCreate
+from testing.conftest import build_short_url_create_random_slug
 
 
 def test_create_short_url(auth_client: TestClient) -> None:
@@ -40,3 +44,39 @@ def test_create_short_url_already_exists(
     response_data = response.json()
     expected_error_detail = f"Short URL with slug={short_url.slug!r} already exists."
     assert response_data["detail"] == expected_error_detail, response_data
+
+
+class TestCreateInvalid:
+
+    @pytest.fixture(
+        params=[
+            pytest.param(("a", "string_too_short"), id="too-short"),
+            pytest.param(("foo-bar-spam-eggs", "string_too_long"), id="too-long"),
+        ],
+    )
+    def short_url_create_values(
+        self,
+        request: SubRequest,
+    ) -> tuple[dict[str, Any], str]:
+        build = build_short_url_create_random_slug()
+        data = build.model_dump(mode="json")
+        slug, err_type = request.param
+        data["slug"] = slug
+        return data, err_type
+
+    def test_invalid_slug(
+        self,
+        short_url_create_values: tuple[dict[str, Any], str],
+        auth_client: TestClient,
+    ) -> None:
+        url = app.url_path_for("create_short_url")
+        create_data, expected_error_type = short_url_create_values
+        response = auth_client.post(
+            url=url,
+            json=create_data,
+        )
+        assert (
+            response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        ), response.text
+        error_detail = response.json()["detail"][0]
+        assert error_detail["type"] == expected_error_type, error_detail
