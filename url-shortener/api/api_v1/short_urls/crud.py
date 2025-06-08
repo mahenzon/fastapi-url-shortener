39ed6fc68217db6ff194c2a39981701e3da8fc9d
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-06-08 23:36:11 +0300

    broken formatting

--- url-shortener/api/api_v1/short_urls/crud.py
+++ url-shortener/api/api_v1/short_urls/crud.py
@@ -57,8 +57,7 @@ class ShortUrlsStorage(BaseModel):
         return [
             ShortUrl.model_validate_json(value)
             for value in cast(
-                Iterable[str],
-                redis.hvals(name=config.REDIS_SHORT_URLS_HASH_NAME),
+                Iterable[str], redis.hvals(name=config.REDIS_SHORT_URLS_HASH_NAME)
             )
         ]
 
@@ -77,7 +76,7 @@ class ShortUrlsStorage(BaseModel):
             bool,
             redis.hexists(
                 name=config.REDIS_SHORT_URLS_HASH_NAME,
-                key=slug,
+                key=slug
             ),
         )
 
