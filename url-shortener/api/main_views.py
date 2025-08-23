dae9b59a66c7df488debd408624d9dffeb2b8b67
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-08-23 23:34:14 +0300

    don't include HTML view in schema

--- url-shortener/api/main_views.py
+++ url-shortener/api/main_views.py
@@ -9,6 +9,7 @@ router = APIRouter()
 @router.get(
     "/",
     response_class=HTMLResponse,
+    include_in_schema=False,
 )
 def read_root() -> str:
     return (BASE_DIR / "pages" / "home.html").read_text()
