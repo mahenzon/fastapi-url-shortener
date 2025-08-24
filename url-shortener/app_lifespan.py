83d40c68b746f4ab01e07f7d2a1b9914a5f718ee
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-08-24 12:22:55 +0300

    global state short urls dependency

--- url-shortener/app_lifespan.py
+++ url-shortener/app_lifespan.py
@@ -3,12 +3,19 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from core.config import settings
+from storage.short_urls import ShortUrlsStorage
+
 
 @asynccontextmanager
 async def lifespan(
-    app: FastAPI,  # noqa: ARG001
+    app: FastAPI,
 ) -> AsyncIterator[None]:
     # действия до запуска приложения
+    # noinspection PyUnresolvedReferences
+    app.state.short_urls_storage = ShortUrlsStorage(
+        hash_name=settings.redis.collections_names.short_urls_hash,
+    )
     # ставим эту функцию на паузу на время работы приложения
     yield
     # выполняем завершение работы,
